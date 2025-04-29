767e93db4b84ff4338436d971dfd1ba8ea0eda49
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2025-04-29 16:48:03 +0200

    Update Lagarto.java

--- Lagarto.java
+++ Lagarto.java
@@ -2,12 +2,12 @@ package interfaces;
 
 public class Lagarto implements Animal {
 
-    @Override
+    
     public void tipoDeAnimal() {
         System.out.println("Soy un reptil");
     }
 
-    @Override
+    
     public String mostrarNombre() {
         return "Lagarto";
     }
