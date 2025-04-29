23926f7b4823540ea069089fab9ed4ae7111ea65
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2025-04-29 16:49:04 +0200

    Update Perro.java

--- Perro.java
+++ Perro.java
@@ -5,12 +5,12 @@ public class Perro implements Animal {
 	
 	//https://docs.oracle.com/javase/tutorial/java/IandI/override.html
     
-	@Override
+	
     public void tipoDeAnimal() {
         System.out.println("Soy un mamifero");
     }
 
-    @Override
+
     public String mostrarNombre() {
         return "Perro";
     }
